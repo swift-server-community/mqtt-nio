0d4557c06435de83b5c1f709d8e9ab9e479fd9b6
Adam Fowler <adamfowler71@gmail.com>
2020-11-19 11:07:35 +0000

    docker-compose for locally run mosquitto

--- Tests/MQTTNIOTests/MQTTNIOTests.swift
+++ Tests/MQTTNIOTests/MQTTNIOTests.swift
@@ -6,69 +6,43 @@ import NIOSSL
 @testable import MQTTNIO
 
 final class MQTTNIOTests: XCTestCase {
-
-    let logger: Logger = {
-        var logger = Logger(label: "MQTTTests")
-        logger.logLevel = .trace
-        return logger
-    }()
-
-    // downloaded from http://test.mosquitto.org/
-    let mosquittoCertificate = """
-    -----BEGIN CERTIFICATE-----
-    MIIEAzCCAuugAwIBAgIUBY1hlCGvdj4NhBXkZ/uLUZNILAwwDQYJKoZIhvcNAQEL
-    BQAwgZAxCzAJBgNVBAYTAkdCMRcwFQYDVQQIDA5Vbml0ZWQgS2luZ2RvbTEOMAwG
-    A1UEBwwFRGVyYnkxEjAQBgNVBAoMCU1vc3F1aXR0bzELMAkGA1UECwwCQ0ExFjAU
-    BgNVBAMMDW1vc3F1aXR0by5vcmcxHzAdBgkqhkiG9w0BCQEWEHJvZ2VyQGF0Y2hv
-    by5vcmcwHhcNMjAwNjA5MTEwNjM5WhcNMzAwNjA3MTEwNjM5WjCBkDELMAkGA1UE
-    BhMCR0IxFzAVBgNVBAgMDlVuaXRlZCBLaW5nZG9tMQ4wDAYDVQQHDAVEZXJieTES
-    MBAGA1UECgwJTW9zcXVpdHRvMQswCQYDVQQLDAJDQTEWMBQGA1UEAwwNbW9zcXVp
-    dHRvLm9yZzEfMB0GCSqGSIb3DQEJARYQcm9nZXJAYXRjaG9vLm9yZzCCASIwDQYJ
-    KoZIhvcNAQEBBQADggEPADCCAQoCggEBAME0HKmIzfTOwkKLT3THHe+ObdizamPg
-    UZmD64Tf3zJdNeYGYn4CEXbyP6fy3tWc8S2boW6dzrH8SdFf9uo320GJA9B7U1FW
-    Te3xda/Lm3JFfaHjkWw7jBwcauQZjpGINHapHRlpiCZsquAthOgxW9SgDgYlGzEA
-    s06pkEFiMw+qDfLo/sxFKB6vQlFekMeCymjLCbNwPJyqyhFmPWwio/PDMruBTzPH
-    3cioBnrJWKXc3OjXdLGFJOfj7pP0j/dr2LH72eSvv3PQQFl90CZPFhrCUcRHSSxo
-    E6yjGOdnz7f6PveLIB574kQORwt8ePn0yidrTC1ictikED3nHYhMUOUCAwEAAaNT
-    MFEwHQYDVR0OBBYEFPVV6xBUFPiGKDyo5V3+Hbh4N9YSMB8GA1UdIwQYMBaAFPVV
-    6xBUFPiGKDyo5V3+Hbh4N9YSMA8GA1UdEwEB/wQFMAMBAf8wDQYJKoZIhvcNAQEL
-    BQADggEBAGa9kS21N70ThM6/Hj9D7mbVxKLBjVWe2TPsGfbl3rEDfZ+OKRZ2j6AC
-    6r7jb4TZO3dzF2p6dgbrlU71Y/4K0TdzIjRj3cQ3KSm41JvUQ0hZ/c04iGDg/xWf
-    +pp58nfPAYwuerruPNWmlStWAXf0UTqRtg4hQDWBuUFDJTuWuuBvEXudz74eh/wK
-    sMwfu1HFvjy5Z0iMDU8PUDepjVolOCue9ashlS4EB5IECdSR2TItnAIiIwimx839
-    LdUdRudafMu5T5Xma182OC0/u/xRlEm+tvKGGmfFcN0piqVl8OrSPBgIlb+1IKJE
-    m/XriWr/Cq4h/JfB7NTsezVslgkBaoU=
-    -----END CERTIFICATE-----
-    """
-
+    
     func createClient() -> MQTTClient {
         MQTTClient(
-            host: "test.mosquitto.org",
+            host: "localhost",
             port: 1883,
             eventLoopGroupProvider: .createNew,
             logger: self.logger
         )
     }
+    
     func createWebSocketClient() -> MQTTClient {
         MQTTClient(
-            host: "test.mosquitto.org",
+            host: "localhost",
             port: 8080,
             eventLoopGroupProvider: .createNew,
             logger: self.logger,
             configuration: .init(useWebSockets: true, webSocketURLPath: "/mqtt")
         )
     }
+
     func createSSLClient() throws -> MQTTClient {
-        let rootCertificate = try NIOSSLCertificate.fromPEMBytes([UInt8](mosquittoCertificate.utf8))
-        let tlsConfiguration: TLSConfiguration? = TLSConfiguration.forClient(
-            trustRoots: .certificates(rootCertificate)
-        )
-        return MQTTClient(
-            host: "test.mosquitto.org",
+        return try MQTTClient(
+            host: "localhost",
             port: 8883,
             eventLoopGroupProvider: .createNew,
             logger: self.logger,
-            configuration: .init(useSSL: true, tlsConfiguration: tlsConfiguration)
+            configuration: .init(useSSL: true, tlsConfiguration: Self.getTLSConfiguration())
+        )
+    }
+
+    func createWebSocketAndSSLClient() throws -> MQTTClient {
+        return try MQTTClient(
+            host: "localhost",
+            port: 8081,
+            eventLoopGroupProvider: .createNew,
+            logger: self.logger,
+            configuration: .init(useSSL: true, useWebSockets: true, tlsConfiguration: Self.getTLSConfiguration(), webSocketURLPath: "/mqtt")
         )
     }
 
@@ -76,9 +50,7 @@ final class MQTTNIOTests: XCTestCase {
         let connect = MQTTConnectInfo(
             cleanSession: true,
             keepAliveSeconds: 15,
-            clientIdentifier: identifier,
-            userName: "",
-            password: ""
+            clientIdentifier: identifier
         )
         try client.connect(info: connect).wait()
     }
@@ -99,11 +71,18 @@ final class MQTTNIOTests: XCTestCase {
         try client.syncShutdownGracefully()
     }
 
+    func testWebsocketAndSSLConnect() throws {
+        let client = try createWebSocketAndSSLClient()
+        try connect(to: client, identifier: "connect")
+        try client.ping().wait()
+        try client.disconnect().wait()
+        try client.syncShutdownGracefully()
+    }
+
     func testMQTTPublishQoS0() throws {
         let publish = MQTTPublishInfo(
             qos: .atMostOnce,
             retain: true,
-            dup: false,
             topicName: "MyTopic",
             payload: ByteBufferAllocator().buffer(string: "Test payload")
         )
@@ -119,12 +98,11 @@ final class MQTTNIOTests: XCTestCase {
         let publish = MQTTPublishInfo(
             qos: .atLeastOnce,
             retain: true,
-            dup: false,
             topicName: "MyTopic",
             payload: ByteBufferAllocator().buffer(string: "Test payload")
         )
 
-        let client = self.createClient()
+        let client = try self.createSSLClient()
         try connect(to: client, identifier: "publisher")
         try client.publish(info: publish).wait()
         try client.disconnect().wait()
@@ -135,12 +113,11 @@ final class MQTTNIOTests: XCTestCase {
         let publish = MQTTPublishInfo(
             qos: .exactlyOnce,
             retain: true,
-            dup: false,
             topicName: "MyTopic",
             payload: ByteBufferAllocator().buffer(string: "Test payload")
         )
         
-        let client = self.createClient()
+        let client = try self.createWebSocketAndSSLClient()
         try connect(to: client, identifier: "soto_publisher")
         try client.publish(info: publish).wait()
         try client.disconnect().wait()
@@ -190,7 +167,6 @@ final class MQTTNIOTests: XCTestCase {
         try connect(to: client2, identifier: "soto_client")
         try client2.subscribe(infos: [.init(qos: .atLeastOnce, topicFilter: "testing-noretain")]).wait()
         try client.publish(info: publish).wait()
-        Thread.sleep(forTimeInterval: 2)
         try client.publish(info: publish).wait()
         Thread.sleep(forTimeInterval: 2)
         XCTAssertEqual(publishReceived.count, 2)
@@ -198,5 +174,46 @@ final class MQTTNIOTests: XCTestCase {
         try client2.disconnect().wait()
         try client.syncShutdownGracefully()
         try client2.syncShutdownGracefully()
+    }
+
+    // MARK: Helper variables and functions
+    
+    let logger: Logger = {
+        var logger = Logger(label: "MQTTTests")
+        logger.logLevel = .trace
+        return logger
+    }()
+
+    static var rootPath: String = {
+        return #file
+            .split(separator: "/", omittingEmptySubsequences: false)
+            .dropLast(3)
+            .map { String(describing: $0) }
+            .joined(separator: "/")
+    }()
+
+    static var _tlsConfiguration: Result<TLSConfiguration, Error> = {
+        do {
+            let rootCertificate = try NIOSSLCertificate.fromPEMFile(MQTTNIOTests.rootPath + "/mosquitto/certs/ca.crt")
+            let certificate = try NIOSSLCertificate.fromPEMFile(MQTTNIOTests.rootPath + "/mosquitto/certs/client.crt")
+            let privateKey = try NIOSSLPrivateKey(file: MQTTNIOTests.rootPath + "/mosquitto/certs/client.key", format: .pem)
+            let tlsConfiguration = TLSConfiguration.forClient(
+                trustRoots: .certificates(rootCertificate),
+                certificateChain: certificate.map{ .certificate($0) },
+                privateKey: .privateKey(privateKey)
+            )
+            return .success(tlsConfiguration)
+        } catch {
+            return .failure(error)
+        }
+    }()
+    
+    static func getTLSConfiguration() throws -> TLSConfiguration {
+        switch _tlsConfiguration {
+        case .success(let config):
+            return config
+        case .failure(let error):
+            throw error
+        }
     }
 }
