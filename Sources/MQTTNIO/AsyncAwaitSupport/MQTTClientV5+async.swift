abb75e32d4eb4834e468544b6000bbd660f2445f
Adam Fowler <adamfowler71@gmail.com>
2021-09-24 09:40:27 +0100

    Use SwiftNIO 2.33.0, Remove _NIOConcurrency imports

--- Sources/MQTTNIO/AsyncAwaitSupport/MQTTClientV5+async.swift
+++ Sources/MQTTNIO/AsyncAwaitSupport/MQTTClientV5+async.swift
@@ -1,7 +1,6 @@
 #if compiler(>=5.5) && canImport(_Concurrency)
 
-import _NIOConcurrency
-import NIO
+import NIOCore
 
 @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
 extension MQTTClient.V5 {
