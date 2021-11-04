13605636a3c1405ed00f6f5e5b8eb1229fc70436
Adam Fowler <adamfowler71@gmail.com>
2021-11-04 13:37:27 +0000

    Username password test (#83)

--- scripts/mosquitto.sh
+++ scripts/mosquitto.sh
@@ -10,12 +10,30 @@ usage()
     exit 2
 }
 
+run()
+{
+    if [ -z "$CONTAINER_ID" ]; then
+        docker run \
+            -p 1883:1883 \
+            -p 1884:1884 \
+            -p 8883:8883 \
+            -p 8080:8080 \
+            -p 8081:8081 \
+            -v $(pwd)/$HOME/../mosquitto/config:/mosquitto/config \
+            -v $(pwd)/$HOME/../mosquitto/certs:/mosquitto/certs \
+            eclipse-mosquitto
+    else
+        echo "Mosquitto is already running"
+    fi
+}
+
 start()
 {
     if [ -z "$CONTAINER_ID" ]; then
         docker run \
             -d \
             -p 1883:1883 \
+            -p 1884:1884 \
             -p 8883:8883 \
             -p 8080:8080 \
             -p 8081:8081 \
@@ -53,6 +71,8 @@ elif [ "$COMMAND" == "stop" ]; then
     stop
 elif [ "$COMMAND" == "status" ]; then
     status
+elif [ -z "$COMMAND" ]; then
+    run
 else
     usage
     exit -1
