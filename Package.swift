8b74993998302b7856ab3e73c16d44e48bd67f3a
jimmy <zhaofeng@shuinfo.com>
2021-11-05 12:50:17 +0800

    update package

--- Package.swift
+++ Package.swift
@@ -9,10 +9,10 @@ let package = Package(
         .library(name: "MQTTNIO", targets: ["MQTTNIO"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.33.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.14.0"),
-        .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.6.0"),
+        .package(url: "https://hub.fastgit.org/apple/swift-log.git", from: "1.0.0"),
+        .package(url: "https://hub.fastgit.org/apple/swift-nio.git", from: "2.33.0"),
+        .package(url: "https://hub.fastgit.org/apple/swift-nio-ssl.git", from: "2.14.0"),
+        .package(url: "https://hub.fastgit.org/apple/swift-nio-transport-services.git", from: "1.6.0"),
     ],
     targets: [
         .target(name: "MQTTNIO", dependencies: [
