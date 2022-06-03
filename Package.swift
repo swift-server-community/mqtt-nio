bc68c7ce00dad7cb369f9090ceb1be8f5a2ee055
Adam Fowler <adamfowler71@gmail.com>
2022-06-03 17:07:29 +0100

    Remove MQTTNIO.docc resource
    This will package MQTTNIO.docc with the library which is undesired

--- Package.swift
+++ Package.swift
@@ -26,8 +26,7 @@ let package = Package(
                 .product(name: "NIOWebSocket", package: "swift-nio"),
                 .product(name: "NIOSSL", package: "swift-nio-ssl", condition: .when(platforms: [.linux, .macOS])),
                 .product(name: "NIOTransportServices", package: "swift-nio-transport-services"),
-            ],
-            resources: [.process("MQTTNIO.docc")]
+            ]
         ),
         .testTarget(name: "MQTTNIOTests", dependencies: ["MQTTNIO"]),
     ]
