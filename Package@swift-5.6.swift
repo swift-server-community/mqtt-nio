d453f472afcdc5ac642c219874db870edb2346ac
Adam Fowler <adamfowler71@gmail.com>
2022-06-05 11:44:06 +0100

    More Package version changes

--- Package@swift-5.6.swift
+++ Package@swift-5.6.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 
 import PackageDescription
 
@@ -13,6 +13,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.33.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.14.0"),
         .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.6.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
@@ -26,8 +27,7 @@ let package = Package(
                 .product(name: "NIOWebSocket", package: "swift-nio"),
                 .product(name: "NIOSSL", package: "swift-nio-ssl", condition: .when(platforms: [.linux, .macOS])),
                 .product(name: "NIOTransportServices", package: "swift-nio-transport-services"),
-            ],
-            exclude: ["MQTTNIO.docc"]
+            ]
         ),
         .testTarget(name: "MQTTNIOTests", dependencies: ["MQTTNIO"]),
     ]
