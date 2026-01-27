d7c84add31f11096faafac7b7637535d84b270c7
Francesco Paolo Severino <96546612+fpseverino@users.noreply.github.com>
2026-01-27 12:53:02 +0100

    Report whether a session is present in `withConnection` closure (#210)
    * Report whether session is present in `withConnection` closure
    
    * Add `withConnection` overload without `sessionPresent`

--- Tests/MQTTNIOTests/MQTTConnectionTests.swift
+++ Tests/MQTTNIOTests/MQTTConnectionTests.swift
@@ -41,7 +41,7 @@ struct MQTTConnectionTests {
         return try await withThrowingTaskGroup { group in
             group.addTask {
                 defer { connection.close() }
-                try await connection.sendConnect()
+                _ = try await connection.sendConnect()
                 try await clientOperation(connection)
             }
             group.addTask {
@@ -497,7 +497,7 @@ struct MQTTConnectionTests {
         return try await withThrowingTaskGroup { group in
             group.addTask {
                 defer { connection.close() }
-                try await connection.sendConnect()
+                _ = try await connection.sendConnect()
             }
             group.addTask {
                 // wait for connect
